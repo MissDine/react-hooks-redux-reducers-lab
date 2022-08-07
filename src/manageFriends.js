fdd8fba6a85ee71cac520971c1e451f7296b817b
MissDine <enidnyatichi1212@gmail.com>
2022-08-07 21:45:42 +0300

    Feature edit

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,11 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action) {
+  switch(action.type){
+    case "friends/add":
+      return { "friends": [...state.friends, action.payload]}
+    case "friends/remove":
+      let new_friends = state.friends.filter((friend) => friend.id !== action.payload)
+      return { "friends": new_friends}
+    default:
+      return state
+  }
 }
